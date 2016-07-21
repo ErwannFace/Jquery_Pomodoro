3c3088ae4b474d616ab0fe068775abaffe18b872
Patrice <patriceguernon@free.fr>
2016-07-22 00:04:15 +0200

    evo algo

--- js/script.js
+++ js/script.js
@@ -1,5 +1,18 @@
-/* * * * * * * * * * * * * * *
- * * * * * * * * * * * * * * *
- * * YOUR CODE GOES HERE ! * *
- * * * * * * * * * * * * * * *
- * * * * * * * * * * * * * * */
+
+
+
+//1. class 'time' : déclencher un compte à rebours de 25 minutes par un clic sur id "play", .console.log() ? 
+// choisir un temps = 25 minutes; 1500secondes !
+//mettre sur pause avec id "pause" et arreter avec id "stop". 
+// créer un RaZ pour remettre au début.
+
+
+
+
+//récuperer la valeur écrite dans input 'doing' pour l'envoyer dans "done" et la barrer.
+
+//SI activité finie
+// SI activté non finie : créer nouvelle fenetre pour utilisateur : 'DOING AGAIN'
+// pour cette fenetre : donner le choix à l'utilisateur du temps. 
+// 5 / 10 ou 15 minutes) avec de simple boutons poussoir pour une "ergonomie" plus simple
+
