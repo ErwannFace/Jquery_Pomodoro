ff68e6e447ef30b55a94f872371f17959330f041
PatdeMessac <patriceguernon@free.fr>
2016-07-22 16:05:40 +0200

    mmm

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
 
+$( "#play").click(function() {
+  $( this ).slideUp();
+});
 
+$( "#pause").click(function() {
+  $( this ).slideUp();
+});
+
+//$('time').fadeIn().delay(150000).fadeOut();
+
+
+	
+
+//$( "p" ).click();
+
+
+// https://www.zirolis.com/tuto-u/R%C3%A9aliser%20un%20chronom%C3%A8tre%20avec%20HTML%20CSS%20et%20JQuery-49
 
 //1. class 'time' : déclencher un compte à rebours de 25 minutes par un clic sur id "play", .console.log() ? 
 // choisir un temps = 25 minutes; 1500secondes !
@@ -15,4 +31,3 @@
 // SI activté non finie : créer nouvelle fenetre pour utilisateur : 'DOING AGAIN'
 // pour cette fenetre : donner le choix à l'utilisateur du temps. 
 // 5 / 10 ou 15 minutes) avec de simple boutons poussoir pour une "ergonomie" plus simple
-
